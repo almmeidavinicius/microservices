465b4ed32693ae2b3667020b7dcea850a6944e49
almmeidavinicius <almmeidavinicius@gmail.com>
2022-12-12 19:51:23 -0300

    Retrieves values from a configuration file

--- greeting-service/src/main/java/br/com/vinicius/controller/GreetingController.java
+++ greeting-service/src/main/java/br/com/vinicius/controller/GreetingController.java
@@ -2,23 +2,31 @@ package br.com.vinicius.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.vinicius.configuration.GreetingConfiguration;
 import br.com.vinicius.model.Greeting;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "%s, %s!";
 	private final AtomicLong counter = new AtomicLong();
+	
+	@Autowired
+	private GreetingConfiguration configuration;
 
 	@GetMapping
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "") String name) {
+		
+		if (name.isEmpty()) name = configuration.getDefaultValue();
+		
+		return new Greeting(counter.incrementAndGet(), String.format(template, configuration.getGreeting(), name));
 	}
 
 }
